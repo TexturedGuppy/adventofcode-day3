d502ba718b35fc9ae770462d66f455b2149ee3b9
Cameron Asbury <CameronAsbury@openarc.net>
2019-12-05 10:58:54 -0500

    Just added in a test tuple to see how the data may look

--- AdventOfCodeDay3/Program.cs
+++ AdventOfCodeDay3/Program.cs
@@ -17,8 +17,13 @@ namespace AdventOfCodeDay3
             int x1, x2, y1, y2;
 
             //Lists that will contain the input coords in the form of strings
-            List<string> wire1 = new List<string>();
-            List<string> wire2 = new List<string>();
+            List<string> wire1Raw = new List<string>();
+            List<string> wire2Raw = new List<string>();
+
+            List<Tuple<int, int>> wireCoords = new List<Tuple<int,int>>();
+            //var wireCoords = new List<Tuple<int,int>>();
+
+
 
             //file the wire inputs come from
             string file = "input.txt";
@@ -55,29 +60,38 @@ namespace AdventOfCodeDay3
                 {
                     for (int i = 0; i < values.Length; i++)
                     {
-                        wire1.Add(values[i]);
+                        wire1Raw.Add(values[i]);
                     }
                 }
                 if (counter == 1)
                 {
                     for (int i = 0; i < values.Length; i++)
                     {
-                        wire2.Add(values[i]);
+                        wire2Raw.Add(values[i]);
                     }
                 }
                 counter++;
             }
 
-            foreach (string coords in wire1)
+            //foreach (string coords in wire1)
+            //{
+            //    Console.WriteLine("Wire 1: " + coords);
+            //}
+
+            //foreach (string coordss in wire2)
+            //{
+            //    Console.WriteLine("Wire 2: " + coordss);
+            //}
+            ////Console.WriteLine(P.ManhattanDistanceFromOrigin(500, 500));
+            wireCoords.Add(new Tuple<int, int>(5, 2));
+            wireCoords.Add(new Tuple<int, int>(4, 1));
+            wireCoords.Add(new Tuple<int, int>(3, 0));
+
+            foreach (Tuple<int,int> i in wireCoords)
             {
-                Console.WriteLine("Wire 1: " + coords);
+                Console.WriteLine(i);
             }
 
-            foreach (string coordss in wire2)
-            {
-                Console.WriteLine("Wire 2: " + coordss);
-            }
-            //Console.WriteLine(P.ManhattanDistanceFromOrigin(500, 500));
 
         }
 
