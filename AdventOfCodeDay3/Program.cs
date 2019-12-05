bdb390fceed6f1e1b8b60c35575b93a3a8886465
Cameron Asbury <CameronAsbury@openarc.net>
2019-12-05 17:35:46 -0500

    added more logic, still broken somewhere

--- AdventOfCodeDay3/Program.cs
+++ AdventOfCodeDay3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCodeDay3
 {
@@ -10,17 +11,14 @@ namespace AdventOfCodeDay3
         {
             //Instantiating a "Program" Object so I can call my functions at the bottom
             Program P = new Program();
-
-            double distanceFromOrigin;
-            double distanceBetweenTwoPoints;
-
-            int x1, x2, y1, y2;
+            int manhattanDistanceFromOrigin = 0;
 
             //Lists that will contain the input coords in the form of strings
             List<string> wire1Raw = new List<string>();
             List<string> wire2Raw = new List<string>();
 
-            List<Tuple<int, int>> wireCoords = new List<Tuple<int,int>>();
+            List<Tuple<int, int>> wire1Coords = new List<Tuple<int,int>>();
+            List<Tuple<int, int>> wire2Coords = new List<Tuple<int,int>>();
             //var wireCoords = new List<Tuple<int,int>>();
 
 
@@ -28,29 +26,9 @@ namespace AdventOfCodeDay3
             //file the wire inputs come from
             string file = "input.txt";
 
+            //Variable used as a way to read in each line of the file and then put each line in a separate list
             int counter = 0;
-            //using (var reader = new StreamReader(file))
-            //{
-                   
-            //    var line = reader.ReadLine();
-            //    var values = line.Split(',');
-            //    if (counter == 0)
-            //    {
-            //        for (int i = 0; i < values.Length; i++)
-            //        {
-            //            wire1.Add(values[i]);
-            //        }
-            //    }
-            //    if (counter == 1)
-            //    {
-            //        for (int i = 0; i < values.Length; i++)
-            //        {
-            //            wire2.Add(values[i]);
-            //        }
-            //    }
-            //    Console.WriteLine(counter);
-            //    counter++;
-            //}
+
 
             //Reads in the two lines to separate lists for parsing later
             foreach (string line in File.ReadLines(file))
@@ -73,25 +51,158 @@ namespace AdventOfCodeDay3
                 counter++;
             }
 
-            //foreach (string coords in wire1)
+            //Just some personal testing of how tuples work
+            //wireCoords.Add(new Tuple<int, int>(5, 2));
+            //wireCoords.Add(new Tuple<int, int>(4, 1));
+            //wireCoords.Add(new Tuple<int, int>(3, 0));
+            //foreach (Tuple<int,int> i in wireCoords)
             //{
-            //    Console.WriteLine("Wire 1: " + coords);
+            //    Console.WriteLine(i);
             //}
 
-            //foreach (string coordss in wire2)
-            //{
-            //    Console.WriteLine("Wire 2: " + coordss);
-            //}
-            ////Console.WriteLine(P.ManhattanDistanceFromOrigin(500, 500));
-            wireCoords.Add(new Tuple<int, int>(5, 2));
-            wireCoords.Add(new Tuple<int, int>(4, 1));
-            wireCoords.Add(new Tuple<int, int>(3, 0));
+            int currentXCoord = 0, currentYCoord = 0;
+            foreach (string s in wire1Raw)
+            {
+                string stringToConvert = s.Substring(1, s.Length - 1);
+                int direction = Convert.ToInt32(stringToConvert);
+                if (s[0] == 'R')
+                {
+                    for (int i = currentXCoord; i <= direction; i++)
+                    {
+                        wire1Coords.Add(new Tuple<int, int>(i, currentYCoord));
 
-            foreach (Tuple<int,int> i in wireCoords)
+                    }
+                    currentXCoord += direction;
+                }
+                if (s[0] == 'L')
+                {
+                    for (int i = currentXCoord; i >= direction * -1; i--)
+                    {
+                        wire1Coords.Add(new Tuple<int, int>(i, currentYCoord));
+
+                    }
+                    currentXCoord -= direction;
+                }
+                if (s[0] == 'U')
+                {
+                    for (int i = currentXCoord; i <= direction; i++)
+                    {
+                        wire1Coords.Add(new Tuple<int, int>(currentXCoord, i));
+
+                    }
+                    currentYCoord += direction;
+                }
+                if (s[0] == 'D')
+                {
+                    for (int i = currentXCoord; i >= direction * -1; i--)
+                    {
+                        wire1Coords.Add(new Tuple<int, int>(currentXCoord, i));
+
+                    }
+                    currentYCoord -= direction;
+                }
+            }
+            currentXCoord = 0;
+            currentYCoord = 0;
+            foreach (string s in wire2Raw)
+            {
+                string stringToConvert = s.Substring(1, s.Length - 1);
+                int direction = Convert.ToInt32(stringToConvert);
+                if (s[0] == 'R')
+                {
+                    for (int i = currentXCoord; i <= direction; i++)
+                    {
+                        wire2Coords.Add(new Tuple<int, int>(i, currentYCoord));
+
+                    }
+                    currentXCoord += direction;
+                }
+                if (s[0] == 'L')
+                {
+                    for (int i = currentXCoord; i >= direction*-1; i--)
+                    {
+                        wire2Coords.Add(new Tuple<int, int>(i, currentYCoord));
+
+                    }
+                    currentXCoord -= direction;
+                }
+                if (s[0] == 'U')
+                {
+                    for (int i = currentXCoord; i <= direction; i++)
+                    {
+                        wire2Coords.Add(new Tuple<int, int>(currentXCoord, i));
+
+                    }
+                    currentYCoord += direction;
+                }
+                if (s[0] == 'D')
+                {
+                    for (int i = currentXCoord; i >= direction * -1; i--)
+                    {
+                        wire2Coords.Add(new Tuple<int, int>(currentXCoord, i));
+
+                    }
+                    currentYCoord -= direction;
+                }
+            }
+
+            IEnumerable<Tuple<int,int>> duplicates = wire1Coords.Intersect(wire2Coords);
+            //var tupleConversion = (Tuple<int,int>)duplicates;
+            //var tupleConversion = duplicates.ToList();
+            int manhattanCounter = 0;
+
+                int tempManhattan = 0;
+            foreach (var i in duplicates)
+            {
+                int x = i.Item1;
+                int y = i.Item2;
+
+                tempManhattan = P.ManhattanDistanceFromOrigin(x, y);
+                Console.WriteLine("before continue");
+                Console.WriteLine("ManhattanCounter Variable: " + manhattanCounter);
+                manhattanCounter++;
+                if (manhattanCounter == 1)
+                {
+                    continue;
+                }
+
+                if (manhattanCounter == 2)
+                {
+                    manhattanDistanceFromOrigin = tempManhattan;
+                }
+
+                else if (tempManhattan < manhattanDistanceFromOrigin)
+                {
+                    manhattanDistanceFromOrigin = tempManhattan;
+                }
+               
+                Console.WriteLine("2nd ManhattanCounter Variable: " + manhattanCounter);
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Shortest Manhattan distance is: " + manhattanDistanceFromOrigin);
+            foreach (var i in duplicates)
             {
                 Console.WriteLine(i);
             }
 
+            //foreach (Tuple<int, int> i in wire1Coords)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //foreach (var i in wire1Coords)
+            //{
+            //    Console.WriteLine(i);
+
+            //}
+            //foreach (var i in wire2Coords)
+            //{
+            //    Console.WriteLine(i);
+
+            //}
+            //reset coords
+            currentXCoord = 0; 
+            currentYCoord = 0;
 
         }
 
